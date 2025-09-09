a872dfa60e5e1d1512549a32f2cec7a4f635b04d
nkampman <156783205+niinaIsHere@users.noreply.github.com>
2025-09-10 01:06:10 +0300

    Add files via upload

---
+++ items.py
@@ -0,0 +1,18 @@
+import db
+
+def add_item():
+    pass
+
+def get_user_items():
+    sql = """SELECT * FROM recipes"""
+    return db.query(sql)
+
+def get_item(item_id):
+    sql = """SELECT * FROM recipes WHERE id == ?"""
+    return db.query(sql, [item_id])[0]
+
+def update_item(item_id, user_id, title, description, menu, skill):
+    sql = """UPDATE recipes
+                SET user_id = ?, title = ?, description = ?, menu = ?, skill = ?
+                WHERE id = ?"""
+    return db.execute(sql, [user_id, title, description, menu, skill, item_id])
