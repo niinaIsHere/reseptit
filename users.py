656da74d7c0e54aab36136aafba212887a933295
nkampman <156783205+niinaIsHere@users.noreply.github.com>
2025-10-04 18:31:25 +0300

    Update users.py

--- users.py
+++ users.py
@@ -1,10 +1,11 @@
 import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 def create_user(username, password):
     password_hash = generate_password_hash(password)
-    sql = """INSERT INTO users (username, password_hash) VALUES (?, ?)"""
-    db.execute(sql, [username, password_hash])
+    sql = """INSERT INTO users (username, password_hash, created) VALUES (?, ?, ?)"""
+    db.execute(sql, [username, password_hash, datetime.now().date().isoformat()])
 
 def check_login(username, password):
     sql = """SELECT id, password_hash FROM users WHERE username = ?"""
@@ -23,6 +24,49 @@ def get_user(user_id):
     sql = """SELECT * FROM users WHERE users.id = ?"""
     return db.query(sql, [user_id])[0]
 
+def get_bio(user_id):
+    sql = """SELECT bio FROM profiles WHERE profiles.user_id = ?"""
+    result = db.query(sql, [user_id])
+    if not result:
+        return None
+    else:
+        return result[0][0]
+
+def update_bio(user_id, content):
+    sql = """INSERT OR REPLACE INTO profiles (user_id, bio) VALUES (?, ?)"""
+    return db.execute(sql, [user_id, content])
+
 def get_comments(user_id):
     sql = """SELECT * FROM comments WHERE user_id = ?"""
+    return db.query(sql, [user_id])
+
+def get_rating(user_id, item_id):
+    sql = """SELECT rating FROM ratings WHERE user_id = ? AND recipe_id = ?"""
+    result = db.query(sql, [user_id, item_id])
+    if not result:
+        return 0
+    return result[0][0]
+
+def get_average_rating(user_id):
+    sql = """SELECT AVG(rating)
+                FROM recipes JOIN ratings ON recipes.id = ratings.recipe_id
+                WHERE recipes.user_id = ?"""
+    result = db.query(sql, [user_id])[0][0]
+    if result:
+        return round(result, 1)
+    else:
+        return None
+
+def recent_activity(user_id):
+    sql = """SELECT title, id, act.type, act.user_id, act.recipe_id, act.content, act.created
+            FROM recipes JOIN
+            (SELECT 'comment' AS type, user_id, recipe_id, content as content, created
+            FROM comments
+            UNION ALL
+            SELECT 'rating' AS type, user_id, recipe_id, rating as content, created FROM ratings) as act
+            ON recipes.id = act.recipe_id
+            WHERE act.user_id = ?
+            GROUP BY type, act.recipe_id, content
+            ORDER BY act.created DESC
+            LIMIT 5"""
     return db.query(sql, [user_id])
